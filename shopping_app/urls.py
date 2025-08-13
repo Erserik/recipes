5d34a0acd3d5afe69b383c2d214fb8b9030d5de6
Erserik <erserikdaryn82@gmail.com>
2025-08-13 20:56:33 +0500

    fix

--- shopping_app/urls.py
+++ shopping_app/urls.py
@@ -3,7 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import ShoppingListViewSet, ShoppingListItemViewSet
 
 router = DefaultRouter()
-router.register(r'', ShoppingListViewSet, basename='shoppinglist')
+router.register(r'shopping-lists', ShoppingListViewSet, basename='shoppinglist')
 router.register(r'items', ShoppingListItemViewSet, basename='shoppinglistitem')
 
 urlpatterns = [path('', include(router.urls))]
+
+
